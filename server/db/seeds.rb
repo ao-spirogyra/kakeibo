86c684a68befd7d6b69de1e446c6f7cc2dd6ffb8
ao-spirogyra <lily.spirogyra@gmail.com>
2021-02-24 08:25:42 +0900

    create seeds to debug

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Expense.create(type: 'aaa', thing: 'aaa', price: 1000, payed_at:Time.now)
