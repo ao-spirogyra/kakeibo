33e91d523a71cd50396005fbf0304f4b59a1eeed
ao-spirogyra <lily.spirogyra@gmail.com>
2021-03-24 08:16:53 +0900

    create sub table

--- client/src/component/TableModal.tsx
+++ client/src/component/TableModal.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { Cell } from './Cell';
+import '../css/TableModal.css';
+import { useExpenseDetails } from '../hooks/useExpenseDetails';
 
 interface TableModalProps{
   show: boolean,
@@ -7,8 +10,40 @@ interface TableModalProps{
 }
 export const TableModal: React.FC<TableModalProps> = ({show, onCloseClicked}: {show, onCloseClicked}) => {
   return (
-    <Modal isOpen={show}>
+    <Modal isOpen={show}
+      ariaHideApp={false}>
       <button onClick={onCloseClicked}>close</button>
+      <SubTable/>
     </Modal>
+  );
+};
+
+const SubTable: React.FC = () => {
+  const { titles, values } = useExpenseDetails();
+  return (
+    <div className='sub-table'>
+      <div className='title-row'>
+        {
+          titles.map((title) => {
+            return <Cell defaultValue={title} key={title}/>;
+          })
+        }
+      </div>
+      <div className='main-row'>
+        {
+          values.map((value) => {
+            return (
+              <div className='each-main-row'key={value}>
+                {
+                  titles.map((title) => {
+                    return <Cell defaultValue={`${value}+${title}`} key={`${value}+${title}`}/>;
+                  })
+                }
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
   );
 };
